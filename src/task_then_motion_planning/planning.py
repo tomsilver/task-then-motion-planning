4b925f217869a03ac65a6fecfdf0ac1f45cc22ea
Tom Silver <tomsilver@users.noreply.github.com>
2024-09-26 09:58:48 -0400

    add task then motion planner and taxi test (#4)

---
+++ src/task_then_motion_planning/planning.py
@@ -0,0 +1,96 @@
+"""Planning interface."""
+
+from typing import Generic
+
+from relational_structs import (
+    GroundOperator,
+    LiftedOperator,
+    PDDLDomain,
+    PDDLProblem,
+    Predicate,
+    Type,
+)
+from relational_structs.utils import parse_pddl_plan
+from tomsutils.pddl_planning import run_pddl_planner
+
+from task_then_motion_planning.structs import Perceiver, Skill, _Action, _Observation
+
+
+class TaskThenMotionPlanningFailure(Exception):
+    """Raised when task then motion planning fails."""
+
+
+class TaskThenMotionPlanner(Generic[_Observation, _Action]):
+    """Run task then motion planning with greedy execution."""
+
+    def __init__(
+        self,
+        types: set[Type],
+        predicates: set[Predicate],
+        perceiver: Perceiver[_Observation],
+        operators: set[LiftedOperator],
+        skills: set[Skill[_Observation, _Action]],
+        planner_id: str = "fd-sat",
+        domain_name: str = "ttmp-domain",
+    ) -> None:
+        self._types = types
+        self._predicates = predicates
+        self._perceiver = perceiver
+        self._operators = operators
+        self._skills = skills
+        self._planner_id = planner_id
+        self._domain_name = domain_name
+        self._domain = PDDLDomain(
+            self._domain_name, self._operators, self._predicates, self._types
+        )
+        self._current_problem: PDDLProblem | None = None
+        self._current_task_plan: list[GroundOperator] = []
+        self._current_operator: GroundOperator | None = None
+        self._current_skill: Skill | None = None
+
+    def reset(self, obs: _Observation) -> None:
+        """Reset on a new task instance."""
+        objects, atoms, goal = self._perceiver.reset(obs)
+        self._current_problem = PDDLProblem(
+            self._domain_name, self._domain_name, objects, atoms, goal
+        )
+        plan_str = run_pddl_planner(
+            str(self._domain), str(self._current_problem), planner=self._planner_id
+        )
+        assert plan_str is not None
+        self._current_task_plan = parse_pddl_plan(
+            plan_str, self._domain, self._current_problem
+        )
+        self._current_operator = None
+        self._current_skill = None
+
+    def step(self, obs: _Observation) -> _Action:
+        """Get an action to execute."""
+        # Get the current atoms.
+        atoms = self._perceiver.step(obs)
+
+        # If the current operator is None or terminated, get the next one.
+        if self._current_operator is None or (
+            self._current_operator.add_effects.issubset(atoms)
+            and not (self._current_operator.delete_effects & atoms)
+        ):
+            # If there is no more plan to execute, fail.
+            if not self._current_task_plan:
+                raise TaskThenMotionPlanningFailure("Empty task plan")
+
+            self._current_operator = self._current_task_plan.pop(0)
+            # Get a skill that can execute this operator.
+            self._current_skill = self._get_skill_for_operator(self._current_operator)
+            self._current_skill.reset(self._current_operator)
+
+        assert self._current_skill is not None
+        return self._current_skill.get_action(obs)
+
+    def _get_skill_for_operator(
+        self, operator: GroundOperator
+    ) -> Skill[_Observation, _Action]:
+        applicable_skills = [s for s in self._skills if s.can_execute(operator)]
+        if not applicable_skills:
+            raise TaskThenMotionPlanningFailure("No skill can execute operator")
+        assert len(applicable_skills) == 1, "Multiple operators per skill not supported"
+        return applicable_skills[0]
---
+++ tests/test_planning.py
@@ -0,0 +1,240 @@
+"""Tests for planning.py."""
+
+from __future__ import annotations
+
+import abc
+from typing import Sequence
+
+import gymnasium as gym
+import numpy as np
+from relational_structs import (
+    GroundAtom,
+    GroundOperator,
+    LiftedOperator,
+    Object,
+    Predicate,
+    Type,
+)
+from tomsutils.search import run_astar
+
+from task_then_motion_planning.planning import TaskThenMotionPlanner
+from task_then_motion_planning.structs import LiftedOperatorSkill, Perceiver
+
+
+def test_task_then_motion_planner():
+    """Tests for TaskThenMotionPlanner()."""
+
+    # Create the environment.
+    env = gym.make("Taxi-v3", render_mode="rgb_array")
+    grid = np.array(
+        [
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0, 0],
+        ]
+    )
+
+    # Uncomment for visualizations.
+    # env = gym.wrappers.RecordVideo(env, "taxi-test")
+
+    # Helper function for parsing observations.
+    dropoff_locs = list(env.unwrapped.locs)
+
+    def _parse_taxi_obs(
+        i: int,
+    ) -> tuple[tuple[int, int], tuple[int, int] | None, tuple[int, int]]:
+        out = []
+        out.append(i % 4)
+        i = i // 4
+        out.append(i % 5)
+        i = i // 5
+        out.append(i % 5)
+        i = i // 5
+        out.append(i)
+        assert 0 <= i < 5
+        taxi_row, taxi_col, pass_loc_idx, dest_idx = reversed(out)
+        taxi_loc = (taxi_row, taxi_col)
+        if pass_loc_idx >= len(dropoff_locs):
+            pass_loc: tuple[int, int] | None = None
+        else:
+            pass_loc = dropoff_locs[pass_loc_idx]
+        dest_loc = dropoff_locs[dest_idx]
+        return taxi_loc, pass_loc, dest_loc
+
+    # Create types.
+    taxi_type = Type("taxi")
+    passenger_type = Type("passenger")
+    destination_type = Type("destination")
+    types = {taxi_type, passenger_type, destination_type}
+
+    # Create predicates.
+    TaxiEmpty = Predicate("TaxiEmpty", [taxi_type])
+    InTaxi = Predicate("InTaxi", [passenger_type, taxi_type])
+    AtDestination = Predicate("AtDestination", [passenger_type, destination_type])
+    predicates = {TaxiEmpty, InTaxi, AtDestination}
+
+    # Create perceiver.
+    class TaxiPerceiver(Perceiver[int]):
+        """A perceiver for the taxi environment."""
+
+        def __init__(self) -> None:
+            self._taxi = taxi_type("taxi")
+            self._passenger = passenger_type("passenger")
+            self._destinations = {
+                (r, c): destination_type(f"dest-{r}-{c}") for r, c in dropoff_locs
+            }
+
+        def reset(
+            self, obs: int
+        ) -> tuple[set[Object], set[GroundAtom], set[GroundAtom]]:
+            objects = {self._taxi, self._passenger} | set(self._destinations.values())
+            atoms, goal = self._parse_observation(obs)
+            return objects, atoms, goal
+
+        def step(self, obs: int) -> set[GroundAtom]:
+            atoms, _ = self._parse_observation(obs)
+            return atoms
+
+        def _parse_observation(
+            self, obs: int
+        ) -> tuple[set[GroundAtom], set[GroundAtom]]:
+            # Unpack the observation.
+            _, pass_loc, dest_loc = _parse_taxi_obs(obs)
+
+            # Create current atoms.
+            atoms: set[GroundAtom] = set()
+            if pass_loc is None:
+                atoms.add(InTaxi([self._passenger, self._taxi]))
+            else:
+                atoms.add(TaxiEmpty([self._taxi]))
+                atoms.add(
+                    AtDestination([self._passenger, self._destinations[pass_loc]])
+                )
+
+            # Create goal atoms.
+            goal = {AtDestination([self._passenger, self._destinations[dest_loc]])}
+
+            return atoms, goal
+
+    perceiver = TaxiPerceiver()
+
+    # Create operators.
+    passenger = passenger_type("?passenger")
+    taxi = taxi_type("?taxi")
+    destination = destination_type("?destination")
+    PickUpOperator = LiftedOperator(
+        "PickUp",
+        [passenger, taxi, destination],
+        preconditions={AtDestination([passenger, destination]), TaxiEmpty([taxi])},
+        add_effects={InTaxi([passenger, taxi])},
+        delete_effects={AtDestination([passenger, destination]), TaxiEmpty([taxi])},
+    )
+
+    DropOffOperator = LiftedOperator(
+        "DropOff",
+        [passenger, taxi, destination],
+        preconditions={InTaxi([passenger, taxi])},
+        add_effects={AtDestination([passenger, destination]), TaxiEmpty([taxi])},
+        delete_effects={InTaxi([passenger, taxi])},
+    )
+    operators = {PickUpOperator, DropOffOperator}
+
+    # Create skills.
+    class TaxiSkill(LiftedOperatorSkill[int, int]):
+        """Shared functionality."""
+
+        def __init__(self) -> None:
+            super().__init__()
+            self._action_queue: list[int] = []
+
+        @abc.abstractmethod
+        def _get_final_action(self) -> int:
+            raise NotImplementedError
+
+        def reset(self, ground_operator: GroundOperator) -> None:
+            self._action_queue = []
+            return super().reset(ground_operator)
+
+        def _get_action_given_objects(self, objects: Sequence[Object], obs: int) -> int:
+            if self._action_queue:
+                return self._action_queue.pop(0)
+
+            taxi_loc, _, _ = _parse_taxi_obs(obs)
+            _, _, destination = objects
+            destination_loc = (
+                int(destination.name.split("-")[1]),
+                int(destination.name.split("-")[2]),
+            )
+            dest_r, dest_c = destination_loc
+            initial_state = taxi_loc
+
+            check_goal = lambda s: s == destination_loc
+
+            delta_to_move_action = {
+                (1, 0): 0,
+                (-1, 0): 1,
+                (0, 1): 2,
+                (0, -1): 3,
+            }
+
+            def get_successors(s):
+                r, c = s
+                for (dr, dc), action in delta_to_move_action.items():
+                    new_row, new_col = r + dr, c + dc
+                    if not (
+                        0 <= new_row < grid.shape[0] and 0 <= new_col < grid.shape[1]
+                    ):
+                        continue
+                    if grid[new_row, new_col]:
+                        continue
+                    yield (action, (new_row, new_col), 1.0)
+
+            def heuristic(s):
+                r, c = s
+                return abs(r - dest_r) + abs(c - dest_c)
+
+            _, actions = run_astar(initial_state, check_goal, get_successors, heuristic)
+            assert actions
+            self._action_queue = list(actions) + [self._get_final_action()]
+
+            return self._action_queue.pop(0)
+
+    class PickUpSkill(TaxiSkill):
+        """Pick up skill."""
+
+        def _get_final_action(self) -> int:
+            return 4  # pick up
+
+        def _get_lifted_operator(self) -> LiftedOperator:
+            return PickUpOperator
+
+    class DropOffSkill(TaxiSkill):
+        """Dropoff skill."""
+
+        def _get_final_action(self) -> int:
+            return 5  # drop off
+
+        def _get_lifted_operator(self) -> LiftedOperator:
+            return DropOffOperator
+
+    skills = {PickUpSkill(), DropOffSkill()}
+
+    # Create the planner.
+    planner = TaskThenMotionPlanner(
+        types, predicates, perceiver, operators, skills, planner_id="pyperplan"
+    )
+
+    # Run an episode.
+    obs, _ = env.reset(seed=123)
+    planner.reset(obs)
+    for _ in range(100):  # should terminate earlier
+        action = planner.step(obs)
+        obs, _, done, _, _ = env.step(action)
+        if done:  # goal reached!
+            break
+    else:
+        assert False, "Goal not reached"
+
+    env.close()
